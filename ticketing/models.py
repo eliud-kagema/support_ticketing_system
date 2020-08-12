271c521ace3a96c34a491eac8498692317b62ca7
Eliud Kagema <eliud.k.maina@gmail.com>
2020-08-12 10:54:19 +0300

    creating models

--- ticketing/models.py
+++ ticketing/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Type(models.Model):
+    type_name = models.CharField(max_length=50)
+    added_by = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.type_name
