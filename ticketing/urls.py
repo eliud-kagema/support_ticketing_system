5856c54f8d43ea77e8ce3806c3bae38ac0cf75d6
Eliud Kagema <eliud.k.maina@gmail.com>
2020-08-12 10:28:16 +0300

    Configurimng the ticketing app

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'ticketing'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('clients/index', views.clients, name='clients_index'),
+    path('support/index', views.support, name='support_index'),
+    path('admin/index', views.administrator, name='admin_index'),
 ]
--- ticketing/views.py
+++ ticketing/views.py
@@ -4,3 +4,22 @@ def index(request):
     context = {}
     template_name = 'index.html'
     return render(request, template_name, context)
+
+
+
+def clients(request):
+    context = {}
+    template_name = 'clients/index.html'
+    return render(request, template_name, context)
+
+
+def support(request):
+    context = {}
+    template_name = 'support/index.html'
+    return render(request, template_name, context)
+
+
+def administrator(request):
+    context = {}
+    template_name = 'admin/index.html'
+    return render(request, template_name, context)
